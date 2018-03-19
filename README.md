caa87de7f2cfb26a023adcac5d8d1c19eb3d8b3e
unknown <erika.ahlberg@medieinstitutet.se>
2018-03-19 10:14:24 +0100

    everything is done

---
+++ README.md
@@ -0,0 +1,9 @@
+# Erika Ahlberg
+# Repository: https://github.com/erikaaahlberg/inlamning_2_ajax
+# Description: An application that allows the user to search for world wide news, sorted by country and category or by a keyword entered by the user.
+# API: https://newsapi.org/ - "Get breaking news headlines, and search for articles from over 30,000 news sources and blogs with our news API".
+# API key: e0a54875bf4f4b4f803131a0b91fc182
+# Languages: JavaScript, css, html
+# TO DO:
+# - Add search functions, the API allows us to search based by sources, by date, or by language as well.
+# - Add sorting functions so that the user can manage the search results they've received.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 landingOnPage();
+
+// Fetching the button related to selectors
 const submitButton = document.getElementById("selectCountrySubmit");
 submitButton.addEventListener("click", function(){
     event.preventDefault();
@@ -9,180 +11,31 @@ submitButton.addEventListener("click", function(){
     printSearchMessage(searchMessage);
     fetchNews(searchParameter);
 }); // submitButton eventlistener collapse
+
+// Fetching the button related to input 
 const searchButton = document.getElementById("anvancedSearchSubmit");
 searchButton.addEventListener("click", function(){
     event.preventDefault();
     const keyword = document.getElementById("advancedSearchInput").value;
     const searchParameter = createParameterFromInput(keyword);
+    saveInLocalStorage("searchMessage", keyword);
+    const searchMessage = getFromLocalStorage("searchMessage");
+    printSearchMessage(searchMessage);
     fetchNews(searchParameter);
-});
+}); // searchButton eventlistener collapse
+
 function landingOnPage(){
     const localStorageStatus = checkLocalStorage("fetchedNews");
-    //localStorage.removeItem("fetchedNews");
-    //localStorage.removeItem("searchParameters");
-    console.log(localStorageStatus);
     if(localStorageStatus){
         const searchMessage = getFromLocalStorage("searchMessage");
-        console.log(searchMessage);
         const news = localStorageStatus;
         if(searchMessage){
             printsearchMessage(searchMessage);
             printNews(news);
-            //printSearchedParameters(searchParameters);
         }
     }
-}
-function removeFromLocalStorage(key){
-    localStorage.removeItem(key);
-}
-function fetchNews(searchParameter) {
-    fetch(`https://newsapi.org/v2/${searchParameter}&apiKey=e0a54875bf4f4b4f803131a0b91fc182`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(fetchedNews) {
-        console.log(fetchedNews);
-        if(fetchedNews && fetchedNews.articles.length > 0){
-            saveInLocalStorage("fetchedNews", fetchedNews);
-            printNews(fetchedNews);
-        }
-        else if(!fetchedNews || fetchedNews.articles.length === 0){
-            removeFromLocalStorage("searchMessage");
-            printErrorMessage("No articles matched your search, please try again!");
-        }
-    })
-    .catch(function(errorMessage) {
-        removeFromLocalStorage("searchMessage");
-        printErrorMessage("Something went wrong, please try again!");
-    }) 
-}
-function checkLocalStorage () {
-    const news = getFromLocalStorage("fetchedNews");
-    if(news){
-        return news;
-    }
-    else{
-        return false;
-    }
-} // checkLocalStorage collapse
-function removeAfter2minutes(key) {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        removeFromLocalStorage(key);
-        location.reload();
-      }, 7000);
-    });
-  }
-function hideItem(item){
-    item.className = "hidden";
-} // hideItem collapse
-async function saveInLocalStorage(key, value){
-    localStorage.setItem(key, JSON.stringify(value));
-    await removeAfter2minutes(key);
-} // saveInLocalStorage collapse
+} // landingOnPage collapse
 
-function getFromLocalStorage(key){
-    const fetchedList = JSON.parse(localStorage.getItem(key));
-    return fetchedList;
-} // getFromLocalStorageCollaps
-
-function printErrorMessage(errorMessage) {
-    const container = document.getElementById("boxSearchNews");
-    const boxErrorMessage = document.createElement("div");
-    const errorMessageParagraph = document.createElement("p");
-    const errorMessageNode = document.createTextNode(errorMessage);
-    errorMessageParagraph.className = "errorMessage";
-    errorMessageParagraph.appendChild(errorMessageNode);
-    boxErrorMessage.appendChild(errorMessageParagraph);
-    container.appendChild(boxErrorMessage);
-    
-    setTimeout(function() {
-        hideItem(boxErrorMessage);
-    }, 3000);
-}
-function printSearchMessage(searchMessage){
-    const container = document.getElementById("boxDisplayNews");
-    const paragraph = document.createElement("h4");
-    paragraph.className = "searchMessage";
-    const parameterNode = document.createTextNode(`You searched for ${searchMessage}`);
-    paragraph.appendChild(parameterNode);
-    container.appendChild(paragraph);
-}
-function editCountrySearchParameters(searchParameter){
-    switch (searchParameter){
-        case "newZealand":
-            return "New Zealand";
-            break;
-        case "southAfrica":
-            return "South Africa";
-            break;
-        case "unitedStates":
-            return "United States";
-            break;
-        default: console.log(searchParameter);
-        return searchParameter;
-    }
-}
-function printNews(news){
-    const container = document.getElementById("boxDisplayNews");
-    container.className = "boxDisplayNews";
-    
-    var i = 0;
-    for (let article of news.articles) {
-        const newsWrapper = document.createElement("div");
-        const newsInfo = document.createElement("h6");
-        const newsTitle = document.createElement("h5");
-        const linkReadMore = document.createElement("a");
-
-        if (i % 2 === 0) {
-            newsWrapper.className = "newsWrapperGrey";
-        }
-        else {
-            newsWrapper.className = "newsWrapper";
-        }
-        
-        newsInfo.className = "newsInfo";
-        newsTitle.className = "newsTitle";
-        linkReadMore.className = "linkReadMore";
-
-        linkReadMore.href = article.url;
-        
-        const infoNode = document.createTextNode(`Published at: ${article.publishedAt} by ${article.source.name}`);
-        const titleNode = document.createTextNode(article.title);
-        const linkNode = document.createTextNode("Read More...");
-
-        linkReadMore.appendChild(linkNode);
-        newsInfo.appendChild(infoNode);
-        newsTitle.appendChild(titleNode);
-        newsWrapper.appendChild(newsInfo);
-        newsWrapper.appendChild(newsTitle);
-        newsWrapper.appendChild(linkReadMore);
-        container.appendChild(newsWrapper);
-
-        i++;
-    }
-    const boxSearchNews = document.getElementById("boxSearchNews");
-    hideItem(boxSearchNews);
-}
-function createParameterFromSelections(selectedCountry,selectedCategory) {
-    const countryCode = countryToFetch(selectedCountry); 
-    const countryParameter = `top-headlines?country=${countryCode}`;
-    const editedCountryParameter = editCountrySearchParameters(selectedCountry);
-
-    if (selectedCategory != "all"){
-        const searchParameter = `${countryParameter}&category=${selectedCategory}`;
-        saveInLocalStorage("searchMessage", `${editedCountryParameter} in category: ${selectedCategory}`);  
-        return searchParameter;
-    }
-    else{
-        saveInLocalStorage("searchMessage", `${selectedCountry}`);  
-        return countryParameter;
-    }
-}
-function createParameterFromInput(keyword){
-    const searchParameter = `everything?q=${keyword}`;
-    return searchParameter;
-}
 function countryToFetch(selectedCountry){
     switch (selectedCountry){
         case "Australia":
@@ -258,5 +111,170 @@ function countryToFetch(selectedCountry){
             return "ve";
             break;
     }
-}
-    //API key: e0a54875bf4f4b4f803131a0b91fc182
+} // countryToFetch collapse
+
+function fetchNews(searchParameter){
+    fetch(`https://newsapi.org/v2/${searchParameter}&apiKey=e0a54875bf4f4b4f803131a0b91fc182`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(fetchedNews) {
+        console.log(fetchedNews);
+        if(fetchedNews && fetchedNews.articles.length > 0){
+            saveInLocalStorage("fetchedNews", fetchedNews);
+            printNews(fetchedNews);
+        }
+        else if(!fetchedNews || fetchedNews.articles.length === 0){
+            // Removes searchMessage from localstorage because it's been saved to localStorage ahead of fetched news
+            removeFromLocalStorage("searchMessage");
+            printErrorMessage("No articles matched your search, please try again!");
+        }
+    })
+    .catch(function(errorMessage){
+        // Removes searchMessage from localstorage because it's been saved to localStorage ahead of fetched news
+        removeFromLocalStorage("searchMessage");
+        printErrorMessage("Something went wrong, please try again!");
+    }) 
+} // fetchNews collapse
+
+function removeAfter3minutes(key){
+    return new Promise(resolve => {
+      setTimeout(() => {
+        removeFromLocalStorage(key);
+        location.reload();
+      }, 30000);
+    });
+} // removeAfter3minutes collapse
+
+function hideItem(item){
+    item.className = "hidden";
+} // hideItem collapse
+
+function checkLocalStorage(){
+    const news = getFromLocalStorage("fetchedNews");
+    if(news){
+        return news;
+    }
+    else{
+        return false;
+    }
+} // checkLocalStorage collapse
+
+async function saveInLocalStorage(key, value){
+    localStorage.setItem(key, JSON.stringify(value));
+    await removeAfter3minutes(key);
+} // saveInLocalStorage collapse
+
+function getFromLocalStorage(key){
+    const fetchedList = JSON.parse(localStorage.getItem(key));
+    return fetchedList;
+} // getFromLocalStorage collapse
+
+function removeFromLocalStorage(key){
+    localStorage.removeItem(key);
+} // removeFromLocalStorage collapse
+
+function printNews(news){
+    const container = document.getElementById("boxDisplayNews");
+    container.className = "boxDisplayNews";
+    
+    var i = 0;
+    for (let article of news.articles) {
+        const newsWrapper = document.createElement("div");
+        const newsInfo = document.createElement("h6");
+        const newsTitle = document.createElement("h5");
+        const linkReadMore = document.createElement("a");
+
+        if (i % 2 === 0) {
+            newsWrapper.className = "newsWrapperGrey";
+        }
+        else {
+            newsWrapper.className = "newsWrapper";
+        }
+        
+        newsInfo.className = "newsInfo";
+        newsTitle.className = "newsTitle";
+        linkReadMore.className = "linkReadMore";
+
+        linkReadMore.href = article.url;
+        
+        const infoNode = document.createTextNode(`Published at: ${article.publishedAt} by ${article.source.name}`);
+        const titleNode = document.createTextNode(article.title);
+        const linkNode = document.createTextNode("Read More...");
+
+        linkReadMore.appendChild(linkNode);
+        newsInfo.appendChild(infoNode);
+        newsTitle.appendChild(titleNode);
+        newsWrapper.appendChild(newsInfo);
+        newsWrapper.appendChild(newsTitle);
+        newsWrapper.appendChild(linkReadMore);
+        container.appendChild(newsWrapper);
+
+        i++;
+    }
+    const boxSearchNews = document.getElementById("boxSearchNews");
+    hideItem(boxSearchNews);
+} // printNews collapse
+
+function printErrorMessage(errorMessage){
+    const container = document.getElementById("boxSearchNews");
+    const boxErrorMessage = document.createElement("div");
+    const errorMessageParagraph = document.createElement("p");
+    const errorMessageNode = document.createTextNode(errorMessage);
+    errorMessageParagraph.className = "errorMessage";
+    errorMessageParagraph.appendChild(errorMessageNode);
+    boxErrorMessage.appendChild(errorMessageParagraph);
+    container.appendChild(boxErrorMessage);
+    
+    setTimeout(function() {
+        hideItem(boxErrorMessage);
+    }, 3000);
+} // printErrorMessage collapse
+
+function printSearchMessage(searchMessage){
+    const container = document.getElementById("boxDisplayNews");
+    const paragraph = document.createElement("h4");
+    paragraph.className = "searchMessage";
+    const parameterNode = document.createTextNode(`You searched for ${searchMessage}`);
+    paragraph.appendChild(parameterNode);
+    container.appendChild(paragraph);
+} // printSearchMessage collapse
+
+// Only these double named parameters needs to be edited before being displayed to the user
+function editCountrySearchParameters(searchParameter){
+    switch (searchParameter){
+        case "newZealand":
+            return "New Zealand";
+            break;
+        case "southAfrica":
+            return "South Africa";
+            break;
+        case "unitedStates":
+            return "United States";
+            break;
+        default:
+        return searchParameter;
+    }
+} // editCountrySearchParameters collapse
+
+function createParameterFromSelections(selectedCountry,selectedCategory) {
+    const countryCode = countryToFetch(selectedCountry); 
+    const countryParameter = `top-headlines?country=${countryCode}`;
+    const editedCountryParameter = editCountrySearchParameters(selectedCountry);
+
+    // Saving searchMessage in localStorage now because it's based on the exact same if statement
+    if (selectedCategory != "all"){
+        const searchParameter = `${countryParameter}&category=${selectedCategory}`;
+        saveInLocalStorage("searchMessage", `${editedCountryParameter} in category: ${selectedCategory}`);  
+        return searchParameter;
+    }
+    else{
+        saveInLocalStorage("searchMessage", `${selectedCountry}`);  
+        return countryParameter;
+    }
+} // createParameterFromSelections collapse
+
+function createParameterFromInput(keyword){
+    const searchParameter = `everything?q=${keyword}`;
+    return searchParameter;
+} // createParameterFromInput collapse
